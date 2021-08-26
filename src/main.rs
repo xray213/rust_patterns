3443298645deffb07aa529dae58dc48110ec3c74
zero <cminquan@outlook.com>
2021-08-26 14:16:17 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn say_hello(name: &str) -> String {
-    format!("Hello {}!", name)
-}
+
+//用format!连接字符串
+//
+// fn say_hello(name: &str) -> String {
+//     format!("Hello {}!", name)
+// }
