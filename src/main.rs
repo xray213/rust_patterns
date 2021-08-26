028ebad01d663c7964639f9e416103313fdfbb1a
zero <cminquan@outlook.com>
2021-08-26 14:43:51 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use std::{path::PathBuf, time::Duration};
+
+// fn main() {
+//     println!("Hello, world!");
+// }
+#[derive(Debug, Default)]
+struct MyConfiguration {
+    output: Option<PathBuf>,
+    search_path: Vec<PathBuf>,
+    timeout: Duration,
+    check: bool,
+}
+impl MyConfiguration {}
+
 fn main() {
-    println!("Hello, world!");
+    let mut conf = MyConfiguration::default();
+    conf.check = true;
+    println!("conf={:#?}", conf);
 }
 
-
-//用format!连接字符串
+//2.2 用format!连接字符串
 //
 // fn say_hello(name: &str) -> String {
 //     format!("Hello {}!", name)
